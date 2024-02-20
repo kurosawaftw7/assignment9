03df7c4912a8d6df574cd802e7b9b3a5acec7c85
kurosawaftw7 <jdharman@gmail.com>
2024-02-20 12:39:25 -0800

    added more files to git

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <NuxtLink to="/" class="nav-link">Jeff</NuxtLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+              </li>
+              <li class="nav-item">
+                <NuxtLink to="/about" class="nav-link">About</NuxtLink>
+              </li>
+              <li class="nav-item">
+                <NuxtLink to="/info" class="nav-link">Info</NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  </NuxtLayout>
+</template>
