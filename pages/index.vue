a6b92ed0c2b865175c268f849ec42f95c99231fb
kurosawaftw7 <jdharman@gmail.com>
2024-02-23 11:54:12 -0800

    fixed img display issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Homepage</h1>
-        <img alt="Vue logo" src="../assets/logo.png">
+        <img alt="Vue logo" src="../assets/assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
   </template>
